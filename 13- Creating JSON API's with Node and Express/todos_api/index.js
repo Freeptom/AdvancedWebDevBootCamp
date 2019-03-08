55296751aa8bf413c1529c2b23558878017eed17
Freeptom <32514405+Freeptom@users.noreply.github.com>
2019-03-08 22:43:04 +0000

    set up mongoose and first routes

--- 13- Creating JSON API's with Node and Express/todos_api/index.js
+++ 13- Creating JSON API's with Node and Express/todos_api/index.js
@@ -2,9 +2,13 @@ let express = require('express'),
     app = express(),
     port = 3000;
 
+let todoRoutes = require('./routes/todos');
+
 app.get('/', function (req, res) {
-    res.send("😀");
-})
+    res.send("😀 Hello from the Root route");
+});
+
+app.use('/api/todos', todoRoutes);
 
 
 
---
+++ 13- Creating JSON API's with Node and Express/todos_api/models/index.js
@@ -0,0 +1,9 @@
+let mongoose = require("mongoose");
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost/todo-api', {
+    useNewUrlParser: true
+});
+
+mongoose.Promise = Promise;
+
+module.exports.Todo = require("./todo");
