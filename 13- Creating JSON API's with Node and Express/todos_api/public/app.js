6ba064a5c131a1a5c5b6d359a3a236c122d24781
Freeptom <32514405+Freeptom@users.noreply.github.com>
2019-03-18 22:05:25 +0000

    deleting todos

--- 13- Creating JSON API's with Node and Express/todos_api/public/app.js
+++ 13- Creating JSON API's with Node and Express/todos_api/public/app.js
@@ -13,47 +13,73 @@ document.addEventListener("DOMContentLoaded", function () {
             // create todo
             createTodo();
         }
-    })
-});
-
-function addTodos(todos) {
-    // add todos to page
-    todos.forEach(function (todo) {
-        addTodo(todo);
     });
-}
 
-function addTodo(todo) {
-    let newTodo = document.createElement('li');
-    newTodo.classList.add('task');
-    let TodoText = document.createTextNode(todo.name);
-    newTodo.appendChild(TodoText);
-    document.querySelector('.list').appendChild(newTodo);
-    if (todo.completed) {
-        newTodo.classList.add('done');
+    let list = document.querySelector('.list');
+    list.addEventListener('click', function (event) {
+        let target = event.target;
+        if (event.target.tagName.toLowerCase() === 'span') {
+            //this.parentNode.removeChild(this);
+            console.log(target.nodeName);
+            console.log(target.parentNode);
+            target.parentNode.remove()
+
+
+        }
+    });
+
+
+    function addTodos(todos) {
+        // add todos to page
+        todos.forEach(function (todo) {
+            addTodo(todo);
+        });
     }
-}
 
-function createTodo() {
-    let userInput = todoInput.value;
-    // send req to create new todo
-    fetch('api/todos/', {
-            method: 'POST',
-            body: JSON.stringify({
-                name: userInput
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (newTodo) {
-            todoInput.value = "";
-            addTodo(newTodo);
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
-}
+    function addTodo(todo) {
+        // create elements
+        let newTodo = document.createElement('li');
+        let newExit = document.createElement('span');
+        // add styling
+        newTodo.classList.add('task');
+        // create text content
+        let TodoText = document.createTextNode(todo.name);
+        let TodoExit = document.createTextNode('X');
+
+        // append content to elements
+        newTodo.appendChild(TodoText);
+        newExit.appendChild(TodoExit);
+        // append span to li
+        newTodo.appendChild(newExit);
+        // append combination to class
+        document.querySelector('.list').appendChild(newTodo);
+        // if completed, style
+        if (todo.completed) {
+            newTodo.classList.add('done');
+        }
+    }
+
+    function createTodo() {
+        let userInput = todoInput.value;
+        // send req to create new todo
+        fetch('api/todos/', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: userInput
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (newTodo) {
+                todoInput.value = "";
+                addTodo(newTodo);
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    }
+});
